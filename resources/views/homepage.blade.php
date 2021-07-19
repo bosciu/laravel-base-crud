34cd2089f2506208be071d189e611af0d016bbe3
bosciu <bosciu@icloud.com>
2021-07-19 15:48:28 +0200

    Preparati fogli di stile e link

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,14 @@
 @extends('layout.main-layout')
+
+@section('asset')
+    @parent
+    <link rel="stylesheet" href="css/homepage.css">
+@endsection
+
+@section('title')
+    Homepage
+@endsection
+
 @section('content')
     <h1>Homepage</h1>
 @endsection
--- resources/views/layout/main-layout.blade.php
+++ resources/views/layout/main-layout.blade.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Comics</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+SC:wght@400;500;600;700;900&display=swap" rel="stylesheet">
+    @section('asset')
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
+    @show
+    <title>Comics | @yield('title')</title>
 </head>
 <body>
     @include('partials.header')
