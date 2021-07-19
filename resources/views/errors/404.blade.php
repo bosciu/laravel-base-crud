e20a3847cf828131caab787776c4ba8bb7831869
bosciu <bosciu@icloud.com>
2021-07-19 15:28:07 +0200

    Gestita pagina errore

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main-layout')
+@section('content')
+    <div class="container">
+        <h1>Ops! Purtroppo sembra esserci un problema, riprova più tardi!</h1>
+    </div>
+@endsection
